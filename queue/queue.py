b35abad7900c8938b756f83e6c80d55d85157c3e
roselandroche <r.landroche@gmail.com>
2020-06-17 20:45:42 -0400

    Completed queue.py - both parts

--- queue/queue.py
+++ queue/queue.py
@@ -13,16 +13,69 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+
+# AS AN ARRAY - FIFO
+
+# class Queue:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+    
+#     def __len__(self):
+#         return len(self.storage)
+
+#     def enqueue(self, value):
+#         # ADD TO END OF QUEUE
+#         return self.storage.append(value)
+
+#     def dequeue(self):
+#         # REMOVE AND RETURN FROM END OF QUEUE
+#         if len(self.storage) > 0:
+#             return self.storage.pop(0)
+#         else:
+#             return None
+
+# AS A LINKED LIST - FIFO
+class Node:
+    def __init__(self, value=None, next_node=None):
+        self.value = value
+        self.next_node = next_node
+
 class Queue:
     def __init__(self):
-        self.size = 0
-        # self.storage = ?
+        self.head = None
+        self.tail = None
     
     def __len__(self):
-        pass
+        if self.head is None and self.tail is None:
+            return 0
+        current_node = self.head
+        length = 1
+        while current_node is not self.tail:
+            length += 1
+            current_node = current_node.next_node
+        return length
 
     def enqueue(self, value):
-        pass
+        new_node = Node(value)
+
+        if self.head is None and self.tail is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next_node = new_node
+            self.tail = new_node
 
     def dequeue(self):
-        pass
+        if not self.head:
+            return None
+        
+        if self.head.next_node is None:
+            head_value = self.head.value
+            self.head = None
+            self.tail = None
+            return head_value
+        
+        head_value = self.head.value
+        self.head = self.head.next_node
+        return head_value
